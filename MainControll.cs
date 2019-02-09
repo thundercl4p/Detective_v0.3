d5d6f0fc3c338b37dd6b2012a7bfb8f78cd26180
Miriadin <47323700+Miriadin@users.noreply.github.com>
2019-02-10 00:13:56 +0300

    v0.3

---
+++ MainControll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MainControll : MonoBehaviour
+{
+    public FixedJoystick MoveJoystick;
+    public FixedTouchField Touchfield;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var fps = GetComponent<RigidbodyFirstPersonController>();
+
+        fps.RunAxis = MoveJoystick.inputVector;
+        fps.mouseLook.LookAxis = Touchfield.TouchDist;
+    }
+}
